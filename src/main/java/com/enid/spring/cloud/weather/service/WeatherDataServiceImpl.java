333ce9ea88663bdad6023ecbbe886f14ff4ee06a
Enid <developer3.logistics@gmail.com>
2019-06-11 22:17:48 +0800

    weather redis

--- src/main/java/com/enid/spring/cloud/weather/service/WeatherDataServiceImpl.java
+++ src/main/java/com/enid/spring/cloud/weather/service/WeatherDataServiceImpl.java
@@ -2,20 +2,31 @@ package com.enid.spring.cloud.weather.service;
 
 import com.enid.spring.cloud.weather.vo.WeatherResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class WeatherDataServiceImpl implements WeatherDataService {
 
+    private final static Logger logger = LoggerFactory.getLogger(WeatherDataServiceImpl.class);
+
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     private final String WEATHER_API = "http://wthrcdn.etouch.cn/weather_mini";
+    private final Long TIME_OUT = 30L;
+
     @Override
     public WeatherResponse getDateByCityId(String cityId) {
         String uri = WEATHER_API + "?citykey=" + cityId;
@@ -29,17 +40,28 @@ public class WeatherDataServiceImpl implements WeatherDataService {
     }
 
     private WeatherResponse doGetWeatherData(String uri) {
-        ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
+        ValueOperations<String, String> ops = this.stringRedisTemplate.opsForValue();
+        String key = uri;
         String strBody = null;
-        if (response.getStatusCodeValue() == 200) {
-            strBody = response.getBody();
+        // 先查缓存，如果没有再查服务
+        if (!this.stringRedisTemplate.hasKey(key)) {
+            logger.info("未找到 key " + key);
+            ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
+            if (response.getStatusCodeValue() == 200) {
+                strBody = response.getBody();
+            }
+            ops.set(key, strBody, TIME_OUT, TimeUnit.SECONDS);
+        } else {
+            logger.info("找到 key " + key + ", value = " + ops.get(key));
+            strBody = ops.get(key);
         }
+
         ObjectMapper mapper = new ObjectMapper();
         WeatherResponse weather = null;
         try {
             weather = mapper.readValue(strBody, WeatherResponse.class);
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error("JSON反序列化异常!", e);
         }
         return weather;
     }
